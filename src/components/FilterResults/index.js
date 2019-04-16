04ca7eafab2c9bcd0ae1024444e1a8924f5f0027
Eric Callari <ericcallari1@gmail.com>
2019-04-15 17:55:00 -0700

    add All button

--- src/components/FilterResults/index.js
+++ src/components/FilterResults/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import store from '../../store/store';
 import handleSearchInputChange from '../../actions/filterResults';
 import '@zendeskgarden/react-buttons/dist/styles.css';
-import { Button } from '@zendeskgarden/react-buttons';
-import { ButtonGroup } from '@zendeskgarden/react-buttons'
+import { Button, ButtonGroup } from '@zendeskgarden/react-buttons';
 
 const FilterResults = (props) => {
     const { searchInput } = props;
@@ -22,8 +21,9 @@ const FilterResults = (props) => {
                 onChange={handleChange}
             />
             <ButtonGroup>
-                <Button key="button-1">Movies</Button>
-                <Button key="button-2">Shows</Button>
+                <Button key="button-1">All</Button>
+                <Button key="button-2">Movies</Button>
+                <Button key="button-3">Shows</Button>
             </ButtonGroup>
         </span>
     );
