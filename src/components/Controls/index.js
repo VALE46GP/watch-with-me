bce4819d8cb01237834118db406282d97990ac8f
Eric Callari <ericcallari1@gmail.com>
2019-04-20 18:24:30 -0700

    convert to react-bootstrap components

--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import store from '../../store/store';
+import './index.css';
 import handleSearchInputChange from '../../actions/filterResults';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 
 const Controls = (props) => {
     const { searchInput } = props;
@@ -11,13 +15,18 @@ const Controls = (props) => {
     };
     return (
         <span>
-            <input
-                className="text-input"
-                type="text"
-                value={searchInput}
-                placeholder="Search..."
-                onChange={handleChange}
-            />
+            <InputGroup className="mb-3">
+                <FormControl
+                    value={searchInput}
+                    placeholder="Filter or Search New"
+                    aria-label="Filter or Search New"
+                    aria-describedby="basic-addon2"
+                    onChange={handleChange}
+                />
+                <InputGroup.Append>
+                    <Button variant="outline-secondary">Search</Button>
+                </InputGroup.Append>
+            </InputGroup>
         </span>
     );
 };
